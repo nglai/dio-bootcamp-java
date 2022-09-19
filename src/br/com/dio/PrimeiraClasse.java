32f1699c8207dc83c005b746efebac6dd962a2ff
Ng Lai <nglaikoin@hotmail.com>
2022-09-19 19:39:09 -0300

    Primeiro commit

---
+++ src/br/com/dio/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Hellou");
+        Gato gato = new Gato();
+    }
+}

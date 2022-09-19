0f02d5ec4965898c0412f0175f078ddf4c5c0dae
Ng Lai <nglaikoin@hotmail.com>
2022-09-19 19:47:32 -0300

    espaco

--- src/br/com/dio/PrimeiraClasse.java
+++ src/br/com/dio/PrimeiraClasse.java
@@ -5,6 +5,7 @@ import br.com.dio.model.Gato;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Hellou");
+
         Gato gato = new Gato();
     }
 }
